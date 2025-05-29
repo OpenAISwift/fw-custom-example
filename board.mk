595518ce12c6e2ceed5ca0584965db8c0fbbc499
Bryan Jimenez <58609628+OpenAISwift@users.noreply.github.com>
2025-05-29 04:44:29 +0000

    update

--- board.mk
+++ board.mk
@@ -9,3 +9,40 @@ include $(BOARD_DIR)/meta-info.env
 # assign critical LED to a non-existent pin if you do not have it on your board
 # good old PD14 is still the default value
 # DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
+
+DDEFS += -DEFI_KLINE=TRUE
+DDEFS += -DSTM32_SERIAL_USE_USART2=TRUE
+
+DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::E3
+DDEFS += -DFIRMWARE_ID=\"proteus\"
+DDEFS += $(VAR_DEF_ENGINE_TYPE)
+
+
+DDEFS += -DEFI_HD_ACR=TRUE
+DDEFS += -DEFI_MAX_31855=TRUE
+DDEFS += -DSTM32_SPI_USE_SPI5=TRUE
+
+DDEFS += -DBOOT_COM_RS232_RX_MAX_DATA=120
+DDEFS += -DOPEN_BLT_TEST_COMMAND=1
+
+# let's start asap
+DDEFS += -DBOOT_BACKDOOR_ENTRY_TIMEOUT_MS=0
+
+# Any Proteus-based adapter boards with discrete-VR decoder are controlled via a 5v ignition output
+DDEFS += -DVR_SUPPLY_VOLTAGE=5
+
+# CAND1
+DDEFS += -DBOOT_COM_CAN_CHANNEL_INDEX=0
+DDEFS += -DOPENBLT_CAN_RX_PORT=GPIOD
+DDEFS += -DOPENBLT_CAN_RX_PIN=0
+DDEFS += -DOPENBLT_CAN_TX_PORT=GPIOD
+DDEFS += -DOPENBLT_CAN_TX_PIN=1
+
+# We are running on Proteus hardware!
+DDEFS += -DHW_PROTEUS=1
+
+DDEFS += -DSTATIC_BOARD_ID=STATIC_BOARD_ID_PROTEUS_F4
+DDEFS += -DRAM_UNUSED_SIZE=100
+# todo: https://github.s/rusefi/rusefi/pull/7505
+# you would think that removal of below like would reduce flash usage but somehow it increased it somehow?!
+DDEFS += -DKNOCK_SPECTROGRAM=TRUE
