c743f8595d5854fcbe9baa374db4bfbeeaec71a0
GitHub git update Action <action@github.com>
2025-05-31 17:49:45 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_custom_proteus_f4.h
+++ generated/controllers/generated/signature_custom_proteus_f4.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 825013733
-#define TS_SIGNATURE "rusEFI main.2025.05.31.custom_proteus_f4.825013733"
+#define SIGNATURE_HASH 128190837
+#define TS_SIGNATURE "rusEFI main.2025.05.31.custom_proteus_f4.128190837"
